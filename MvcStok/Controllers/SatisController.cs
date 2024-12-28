f2a266fa08913fa10442a5d9800e96d226a83ef8
ahmeteke00 <eke.ahmet72@gmail.com>
2024-12-28 15:36:49 +0300

    Initial commit

---
+++ MvcStok/Controllers/SatisController.cs
@@ -0,0 +1,23 @@
+﻿using MvcStok.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcStok.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+
+
+
+    }
+}
